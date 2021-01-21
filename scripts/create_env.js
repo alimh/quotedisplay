79972cb6d24a065fb09f55e99d83674e50c0ec12
Alim Haji <alim.haji@gmail.com>
2021-01-20 19:27:33 -0700

    Move to .env

--- scripts/create_env.js
+++ scripts/create_env.js
@@ -1,2 +1,2 @@
 var fs = require('fs');
-fs.writeFileSync('./env.js', `{ module.exports = {API_KEY=${process.env.API_KEY}, CLIENT_ID=${process.env.CLIENT_ID}\n`);
+fs.writeFileSync('./.env', `var API_KEY=${process.env.API_KEY};\nvar CLIENT_ID=${process.env.CLIENT_ID};\n`);
